2925ce9608f25a4c125347ca868bde71c3d41271
Mateus <mateusdeveloper@outlook.com>
2025-11-03 12:53:21 -0300

    hashashas aprendi criar meu primeiro programa em C.

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("Olá, Mundo!\n");
+    getchar(); // espera o usuário pressionar Enter para fechar.
+    //system("pause"); // exibe "Pressione qualquer tecla para continuar..." e espera
+    return 0;
+}
+
+
